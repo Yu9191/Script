6c244bf1dc3f9e3ae0aec2ee67152fe686f94dd5
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-11-01 16:11:56 +0800

    Update m3u8.js

--- m3u8.js
+++ m3u8.js
@@ -41,6 +41,9 @@
 
 ^https?://120play\.zihzot\.com/videos2/\w{32}/\w{32}\.m3u8\?auth_key=\d+-\d+-\d+-[\w-]{32}&v=\d+$ url script-request-header https://raw.githubusercontent.com/Yu9191/Script/main/m3u8.js
 
+#通用
+#^https?://\w+\.zihzot\.com/videos2/\w{32}/\w{32}\.m3u8\?auth_key=\d+-\d+-\d+-[\w-]{32}&v=\d+$ url script-request-header https://raw.githubusercontent.com/Yu9191/Script/main/m3u8.js
+
 [mitm] 
 
 hostname = *.zihzot.com
