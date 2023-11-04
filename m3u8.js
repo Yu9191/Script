18dbd0499f26d91e01a1e7a4a24c62aefb598206
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-11-04 13:19:01 +0800

    Update m3u8.js

--- m3u8.js
+++ m3u8.js
@@ -4,16 +4,47 @@
  * 这里只是汤头条播放地址为例，其他的还有50度灰也是h5play，91短视频（通杀解锁vip视频），海角视频俩是10play，海角金币视频是12play
  * 妻友,糖人是120play，糖人部分视频h5play，91AV是120play，91AV部分视频10play,微密圈10play
  * 小蓝视频h5play，她趣-首页视频h5play，她趣-小视频10play，
- * 汤头条在线观看：https://p4.ttt010.biz/下载地址：http://ttt.tips/ 备用https://df.fnyxpk.com/chan/a11954/cvDh9 
- * 海角社区在线地址：https://p1.haij010.biz/ 下载地址：https://haijiao.fun/
- * 50度灰在线视频：https://p3.hui010.biz/ 下载地址：http://50dh.app/
- * 妻友在线视频：https://uapp.live/ 备用地址：https://4ee.neflgo.com/?code=RJdm
- * 糖人在线观看：https://tren.live/备用地址：https://tren.app002.biz/af/cduf
- * 91短视频：https://dbf4801.zfijby.com/aff-hVbkx
- * 91AV：https://mv.aff009.net/aff-cmAru
- * 微密圈：https://app.wmq.life/aff-nVJM
- * 小蓝视频：https://p4.gv009.fun/#/home
- * 她趣视频：https://app.xttworld.com/af/bnjU
+ * 缅北禁地  https://ac9d.wykfnp.com
+暗网解密  https://714.ndqvyg.com
+51吃瓜Pro  https://3d4f.knjbzw.com
+51猎奇  https://088f.bjzezy.com
+私房ktv  https://89.rwhmcn.com
+天涯社区  https://f3d.wchgdn.com
+黑洞社区  https://ca64.mdplzw.com
+暗网禁区  https://0b.yrhwse.com
+她趣  https://5e7.rtlzhe.com
+Blued  https://5ba.aryhca.com
+妹团  https://36.paxvrh.com
+杏吧  https://9923.hzbwia.com
+老九品茶  https://0a.obntac.com
+微密圈  https://a4.henwap.com
+海角乱伦社区  https://f58d.ipqjlb.com
+51动漫  https://75.luduwz.com
+蜜豆视频  https://6de.scrrzr.com
+黑料天堂  https://42fa.jaedlj.com
+51吃瓜  https://lz7as.zzttja.com
+新版滴滴  https://cf.sjlqjh.com
+七度少女  https://11a.uvmtsn.com
+91妻友  https://08c0.lvxpha.com
+91制片厂Pro  https://baf.dhlwyz.com
+抖阴Proj  https://6f7s.pbgrzh.com
+果冻传媒APP  https://07.npcwwc.com
+福利姬  https://9f.gectab.com
+91fans  https://fc.zlnkbn.com
+pilipili(二次元)  https://747.jkhpmy.com
+歪歪视频  https://c8.qznpyr.com
+GTV  https://958.xqhjej.com
+91视频-全能版  https://fbrg.rzvwcq.com
+撸先生  https://01f4.czhxnw.com
+成人B站  https://d4.qhzvgn.com
+小蓝俱乐部  https://ca.sbnyxu.com
+50度灰  https://od5em.smpaqa.com
+51品茶  https://f5f.vavkyj.com
+蚂蚁翻墙  https://636d.obqyyh.com
+91短视频  https://66.kccber.com
+91AV  https://ca7.rahzss.com
+快手  https://eb81.nnjzpw.com
+汤头条  https://a3ou.cxuvtk.com
  * 在QuanX中运行的脚本 同时支持 QuanX 和 Loon 两种格式, EnvJs根据运行环境自动转换, Surge 环境不支持多媒体通知
  * 用于捕获视频链接并显示通知
  * 适用于QuanX APP版本 1.0.10-build198及以上
